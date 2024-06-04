63262ad1dea4dcd44fc92ca19cecdfc3c7fe609a
jojayeon <jojayeon6152@gmail.com>
2024-06-04 10:56:15 +0900

    a+b 밖에서 처리함

--- work-2.js
+++ work-2.js
@@ -7,13 +7,23 @@ class WorkTwo {
     if(typeof(value) === "number"){
       return this._a = value;
     }else {
-      throw new Error("a는 숫자만 입력 가능합니다.")//코드 멈춤
+      throw new Error("a는 숫자만 입력 가능합니다.");//코드 멈춤
+    }
+  }
+  set b(value) {
+    if(typeof(value) === "number"){
+      return this._b = value;
+    }else {
+      throw new Error("b는 숫자만 입력 가능합니다.");//코드 멈춤
     }
   }
 }
 
 
 let test = new WorkTwo(1,2);
-let test2 = new WorkTwo("w",2);
+// let test2 = new WorkTwo("w","c");
+// let test3 = new WorkTwo(1,"c");
 console.log(test);
-console.log(test2);
+console.log(test._a + test._b);
+// console.log(test2);
+// console.log(test3);
