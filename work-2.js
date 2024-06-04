7dbcae388094c779b859c85735d90b8ba7c51d30
jojayeon <jojayeon6152@gmail.com>
2024-06-04 10:48:29 +0900

    work-2 set

--- work-2.js
+++ work-2.js
@@ -3,8 +3,15 @@ class WorkTwo {
     this.a = a;
     this.b = b;
   }
+  set a(value) {
+    if(typeof(value) === "number"){
+      return this._a = value;
+    }
+  }
 }
 
 
 let test = new WorkTwo(1,2);
+let test2 = new WorkTwo("w",2);
 console.log(test);
+console.log(test2);
