506721a3ece7ce2e12c5860b7959517bf4a2ae5d
jojayeon <jojayeon6152@gmail.com>
2024-06-04 13:01:31 +0900

    class작성

---
+++ class.js
@@ -0,0 +1,13 @@
+class ItsMe {
+  constructor() {
+    this.name = "조자연",
+    this.age = 26,
+    this.home = "계룡"
+  }
+  sentence() {
+    return this.name + ", 앤디";
+  }
+}
+
+let itsme = new ItsMe();
+console.log(itsme);
