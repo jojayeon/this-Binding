d818ea0b8ade690e1b4ec4772e68ee98b4f3ea1b
jojayeon <jojayeon6152@gmail.com>
2024-06-04 11:03:08 +0900

    add()메서드 생성

--- work-2.js
+++ work-2.js
@@ -17,6 +17,10 @@ class WorkTwo {
       throw new Error("b는 숫자만 입력 가능합니다.");//코드 멈춤
     }
   }
+  add() {
+    return this._a + this._b;
+  }
+
 }
 
 
@@ -24,6 +28,8 @@ let test = new WorkTwo(1,2);
 // let test2 = new WorkTwo("w","c");
 // let test3 = new WorkTwo(1,"c");
 console.log(test);
-console.log(test._a + test._b);
+// console.log(test._a + test._b);
+console.log(test.add());
+
 // console.log(test2);
 // console.log(test3);
